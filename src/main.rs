76f9b06fc1bc5dbec80dc544853caeac6d12fee2
Sebastian <stmobo@gmail.com>
2020-02-27 19:39:43 -0500

    Continue work with VGA text-mode interface, add panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
 use core::panic::PanicInfo;
 
 mod vga;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[allow(unused_must_use)]
+fn panic(info: &PanicInfo) -> ! {
+    use core::fmt::Write;
+
+    let mut disp = unsafe { vga::force_take_lock() };
+    write!(disp, "kernel panic: ");
+
+    if let Some(msg) = info.message() {
+        write!(disp, "{}", msg);
+    } else if let Some(msg) = info.payload().downcast_ref::<&'static str>() {
+        write!(disp, "{}", msg);
+    } else {
+        write!(disp, "(no message provided)");
+    }
+
+    if let Some(loc) = info.location() {
+        write!(disp, " at {}\n", loc);
+    } else {
+        write!(disp, " - no location information available\n");
+    }
+
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello world{}", "!");
+    panic!("test");
+
     loop {}
 }
--- src/vga.rs
+++ src/vga.rs
@@ -1,7 +1,20 @@
 use core::ptr;
+use core::fmt;
+
+use lazy_static::lazy_static;
+use spin::{Mutex, MutexGuard};
+
+const SCREEN_WIDTH: u64  = 80;
+const SCREEN_HEIGHT: u64 = 25;
+const DEFAULT_COLOR: Color = Color(0x0F);
+
+lazy_static! {
+    pub static ref DEFAULT_DISPLAY: Mutex<VGATextMode> = Mutex::new(VGATextMode::new(0xB8000));
+}
 
 #[derive(Clone, Debug, Eq, PartialEq, Copy)]
 #[repr(u8)]
+#[allow(dead_code)]
 pub enum Palette {
     Black        = 0,
     Blue         = 1,
@@ -22,9 +35,10 @@ pub enum Palette {
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
-struct Color(u8);
+pub struct Color(u8);
 
 impl Color {
+    #[allow(dead_code)]
     pub fn new(fg: Palette, bg: Palette) -> Color {
         Color(((bg as u8) << 4) | fg as u8)
     }
@@ -37,56 +51,144 @@ struct VGACharacter {
     color: Color,
 }
 
-const SCREEN_WIDTH: usize  = 25;
-const SCREEN_HEIGHT: usize = 80;
-const DEFAULT_COLOR: Color = Color(0x0F);
-
 pub struct VGATextMode {
-    buf: *mut u8, 
+    buf: &'static mut [VGACharacter],
+    cur_x: u64,
+    cur_y: u64,
 }
 
 impl VGATextMode {
-    fn new(base_addr: *mut u8) -> VGATextMode {
+    fn new(base_addr: usize) -> VGATextMode {
+        let sz: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+        let base = base_addr as *mut VGACharacter;
+
         VGATextMode {
-            buf: base_addr 
+            buf: unsafe { &mut *ptr::slice_from_raw_parts_mut(base, sz) },
+            cur_x: 0,
+            cur_y: 0,
         }
     }
 
-    pub fn set_color(&mut self, x: usize, y: usize, color: Color) {
+    fn offset (x: u64, y: u64) -> Option<usize> {
         if x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT {
-            return;
-        }
-
-        let offset: isize = 2 * (x + (y * SCREEN_WIDTH)) as isize;
-        unsafe {
-            ptr::write_volatile(self.buf.offset(offset), color.0);
+            None
+        } else {
+            Some((x + (y * SCREEN_WIDTH)) as usize)
         }
     }
 
-    pub fn set_glyph(&mut self, x: usize, y: usize, glyph: u8) {
-        if x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT {
-            return;
-        }
+    /// Set the color of a particular cell on screen.
+    pub fn set_color (&mut self, x: u64, y: u64, color: Color) {
+        if let Some(offset) = VGATextMode::offset(x, y) {
+            self.buf[offset].color = color;
+        }        
+    }
 
-        let offset: isize = 2 * (x + (y * SCREEN_HEIGHT)) as isize;
-        unsafe {
-            ptr::write_volatile(self.buf.offset(offset+1), glyph);
+    /// Set the displayed glyph for a particular cell on screen.
+    pub fn set_glyph (&mut self, x: u64, y: u64, glyph: u8) {
+        if let Some(offset) = VGATextMode::offset(x, y) {
+            self.buf[offset].glyph = glyph;
         }
     }
 
-    pub fn write_char_color(&mut self, x: usize, y: usize, c: char, color: Color) {
-        if !c.is_ascii() {
+    /// Put a character on-screen with a specified color.
+    pub fn put_char_color (&mut self, x: u64, y: u64, glyph: u8, color: Color) {
+        if let Some(offset) = VGATextMode::offset(x, y) {
+            self.buf[offset] = VGACharacter {
+                color,
+                glyph
+            }
+        }
+    }
+
+    /// Put a character on-screen with a default color.
+    pub fn put_char (&mut self, x: u64, y: u64, glyph: u8) {
+        self.put_char_color(x, y, glyph, DEFAULT_COLOR);
+    }
+
+    /// Scroll the display upwards.
+    pub fn scroll (&mut self, lines: u64) {
+        if lines == 0 || lines >= SCREEN_HEIGHT {
             return;
         }
 
-        let mut b = [0; 4];
-        c.encode_utf8(&mut b);
-        self.set_color(x, y, color);
-        self.set_glyph(x, y, b[0]);
+        let copy_start = (SCREEN_WIDTH * lines) as usize;
+        self.buf.copy_within(copy_start..self.buf.len(), 0);
+
+        let clear_start = ((SCREEN_HEIGHT - lines) * SCREEN_WIDTH) as usize;
+        for c in self.buf.iter_mut().skip(clear_start) {
+            *c = VGACharacter {
+                color: DEFAULT_COLOR,
+                glyph: 0x20, // ASCII space
+            }
+        }
     }
 
-    pub fn write_char(&mut self, x: usize, y: usize, c: char) {
-        self.write_char_color(x, y, c, DEFAULT_COLOR);
+    fn newline (&mut self) {
+        if self.cur_y >= SCREEN_HEIGHT - 1 {
+            self.cur_y = SCREEN_HEIGHT - 1;
+            self.scroll(1);
+        } else {
+            self.cur_y += 1;
+        }
+
+        self.cur_x = 0;
+    }
+
+    /// Write a character to the screen.
+    pub fn write_char(&mut self, c: u8) {
+        match c {
+            b'\n' => self.newline(),
+            c => {
+                if self.cur_x >= SCREEN_WIDTH {
+                    self.newline()
+                }
+
+                self.put_char(self.cur_x, self.cur_y, c);
+                self.cur_x += 1;
+            }
+        }
+    }
+
+    /// Write a string to the screen.
+    pub fn write_str(&mut self, s: &str) {
+        for c in s.bytes() {
+            match c {
+                (0x20..=0x7E) | b'\n' => self.write_char(c),
+                _ => self.write_char(0xFE)
+            };
+        }
     }
 }
 
+impl fmt::Write for VGATextMode {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        VGATextMode::write_str(self, s);
+        Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga::_do_blocking_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _do_blocking_print(args: fmt::Arguments) {
+    use fmt::Write;
+    DEFAULT_DISPLAY.lock().write_fmt(args).unwrap();
+}
+
+/// Forcibly take the lock for the default VGA display.
+/// 
+/// Note that this is horrifically unsafe.
+pub unsafe fn force_take_lock() -> MutexGuard<'static, VGATextMode> {
+    DEFAULT_DISPLAY.force_unlock();
+    DEFAULT_DISPLAY.lock()
+}

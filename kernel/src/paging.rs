21fc4e3e6c5b6f005701c6cce95cdc21e3994806
Sebastian <stmobo@gmail.com>
2020-03-08 01:31:54 -0500

    Add new enum for paging structure indices

--- kernel/src/paging.rs
+++ kernel/src/paging.rs
@@ -87,30 +87,22 @@ impl PageTable {
     }
 
     /// Get a reference to a recursively-mapped PD Pointer Table.
-    pub fn get_pdpt(pdp_idx: usize) -> &'static mut PageTable {
-        unsafe {
-            let pdp_addr = PDPT_RECURSIVE_BASE + (0x1000 * pdp_idx);
-            &mut *(pdp_addr as *mut PageTable)
-        }
+    unsafe fn get_pdpt(pdp_idx: usize) -> &'static mut PageTable {
+        let pdp_addr = PDPT_RECURSIVE_BASE + (0x1000 * pdp_idx);
+        &mut *(pdp_addr as *mut PageTable)
     }
 
     /// Get a reference to a recursively-mapped Page Directory.
-    pub fn get_pd(pdp_idx: usize, pd_idx: usize) -> &'static mut PageTable {
-        unsafe {
-            let pd_addr = PD_RECURSIVE_BASE + (0x20_0000 * pdp_idx) + (0x1000 * pd_idx);
-            &mut *(pd_addr as *mut PageTable)
-        }
+    unsafe fn get_pd(pdp_idx: usize, pd_idx: usize) -> &'static mut PageTable {
+        let pd_addr = PD_RECURSIVE_BASE + (0x20_0000 * pdp_idx) + (0x1000 * pd_idx);
+        &mut *(pd_addr as *mut PageTable)
     }
 
     /// Get a reference to a recursively-mapped Page Table.
-    pub fn get_pt(pdp_idx: usize, pd_idx: usize, pt_idx: usize) -> &'static mut PageTable {
-        unsafe {
-            let pt_addr = PT_RECURSIVE_BASE
-                + (0x4000_0000 * pdp_idx)
-                + (0x20_0000 * pd_idx)
-                + (0x1000 * pt_idx);
-            &mut *(pt_addr as *mut PageTable)
-        }
+    unsafe fn get_pt(pdp_idx: usize, pd_idx: usize, pt_idx: usize) -> &'static mut PageTable {
+        let pt_addr =
+            PT_RECURSIVE_BASE + (0x4000_0000 * pdp_idx) + (0x20_0000 * pd_idx) + (0x1000 * pt_idx);
+        &mut *(pt_addr as *mut PageTable)
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &PageTableEntry> {
@@ -136,6 +128,128 @@ impl ops::IndexMut<usize> for PageTable {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialOrd, PartialEq, Eq, Ord)]
+pub enum PageHierarchyIndex {
+    PML4T,
+    PDPT(usize),
+    PD(usize, usize),
+    PT(usize, usize, usize),
+}
+
+impl PageHierarchyIndex {
+    /// Check to see if all of this index's structure indices are valid.
+    pub fn is_valid(&self) -> bool {
+        match &self {
+            PageHierarchyIndex::PML4T => true,
+            PageHierarchyIndex::PDPT(i1) => *i1 <= 512,
+            PageHierarchyIndex::PD(i1, i2) => *i1 <= 512 && *i2 <= 512,
+            PageHierarchyIndex::PT(i1, i2, i3) => *i1 <= 512 && *i2 <= 512 && *i3 <= 512,
+        }
+    }
+
+    /// Get an index for the page table corresponding to a given
+    /// virtual address.
+    pub fn from_vaddr(virt_addr: usize) -> PageHierarchyIndex {
+        PageHierarchyIndex::PT(
+            (virt_addr >> 39) & 0x1FF,
+            (virt_addr >> 30) & 0x1FF,
+            (virt_addr >> 21) & 0x1FF,
+        )
+    }
+
+    /// Gets a reference to a page table without checking if indices are
+    /// valid or if higher-level paging structures are mapped.
+    pub unsafe fn get_table_unchecked(self) -> &'static mut PageTable {
+        match self {
+            PageHierarchyIndex::PML4T => PageTable::get_pml4t(),
+            PageHierarchyIndex::PDPT(pml4t_idx) => PageTable::get_pdpt(pml4t_idx),
+            PageHierarchyIndex::PD(pml4t_idx, pdpt_idx) => PageTable::get_pd(pml4t_idx, pdpt_idx),
+            PageHierarchyIndex::PT(pml4t_idx, pdpt_idx, pd_idx) => {
+                PageTable::get_pt(pml4t_idx, pdpt_idx, pd_idx)
+            }
+        }
+    }
+
+    /// Consume this index and get an index for its parent PDPT.
+    pub fn associated_pdpt(self) -> Option<PageHierarchyIndex> {
+        if !self.is_valid() {
+            return None;
+        }
+
+        let pml4t = PageTable::get_pml4t();
+        match self {
+            PageHierarchyIndex::PML4T => None,
+            PageHierarchyIndex::PDPT(pml4t_idx)
+            | PageHierarchyIndex::PD(pml4t_idx, _)
+            | PageHierarchyIndex::PT(pml4t_idx, _, _) => Some(PageHierarchyIndex::PDPT(pml4t_idx)),
+        }
+    }
+
+    /// Consume this index and get an index for its parent page directory.
+    pub fn pd_index(self) -> Option<PageHierarchyIndex> {
+        match self {
+            PageHierarchyIndex::PML4T | PageHierarchyIndex::PDPT(_) => None,
+            PageHierarchyIndex::PD(pml4t_idx, pdpt_idx)
+            | PageHierarchyIndex::PT(pml4t_idx, pdpt_idx, _) => {
+                Some(PageHierarchyIndex::PD(pml4t_idx, pdpt_idx))
+            }
+        }
+    }
+
+    /// Get the actual paging structure this index refers to.
+    pub fn get_table(self) -> Option<&'static mut PageTable> {
+        if !self.is_valid() {
+            return None;
+        }
+
+        let pml4t = PageTable::get_pml4t();
+        match self {
+            PageHierarchyIndex::PML4T => {
+                return Some(pml4t);
+            }
+            PageHierarchyIndex::PDPT(pml4t_idx) => {
+                let pte: &PageTableEntry = &pml4t[pml4t_idx];
+                if pte.present() {
+                    return unsafe { Some(PageTable::get_pdpt(pml4t_idx)) };
+                }
+
+                return None;
+            }
+            PageHierarchyIndex::PD(pml4t_idx, pdpt_idx) => {
+                let pte: &PageTableEntry = &pml4t[pml4t_idx];
+                if pte.present() {
+                    let pdpt = unsafe { PageTable::get_pdpt(pml4t_idx) };
+                    let pte = pdpt[pdpt_idx];
+
+                    if pte.present() {
+                        return unsafe { Some(PageTable::get_pd(pml4t_idx, pdpt_idx)) };
+                    }
+                }
+
+                return None;
+            }
+            PageHierarchyIndex::PT(pml4t_idx, pdpt_idx, pd_idx) => {
+                let pte: &PageTableEntry = &pml4t[pml4t_idx];
+                if pte.present() {
+                    let pdpt = unsafe { PageTable::get_pdpt(pml4t_idx) };
+                    let pte = pdpt[pdpt_idx];
+
+                    if pte.present() {
+                        let pd = unsafe { PageTable::get_pd(pml4t_idx, pdpt_idx) };
+                        let pte = pd[pd_idx];
+
+                        if pte.present() {
+                            return unsafe { Some(PageTable::get_pt(pml4t_idx, pdpt_idx, pd_idx)) };
+                        }
+                    }
+                }
+
+                return None;
+            }
+        }
+    }
+}
+
 pub fn get_mapping(virt_addr: usize) -> Option<PageTableEntry> {
     if (virt_addr & 0xFFF) > 0 {
         return None;
@@ -148,13 +262,13 @@ pub fn get_mapping(virt_addr: usize) -> Option<PageTableEntry> {
 
     let pml4t = PageTable::get_pml4t();
     if pml4t[pml4_idx].present() {
-        let pdpt = PageTable::get_pdpt(pml4_idx);
+        let pdpt = unsafe { PageTable::get_pdpt(pml4_idx) };
 
         if pdpt[pdp_idx].present() {
-            let pd = PageTable::get_pd(pml4_idx, pdp_idx);
+            let pd = unsafe { PageTable::get_pd(pml4_idx, pdp_idx) };
 
             if pd[pd_idx].present() {
-                let pt = PageTable::get_pt(pml4_idx, pdp_idx, pd_idx);
+                let pt = unsafe { PageTable::get_pt(pml4_idx, pdp_idx, pd_idx) };
 
                 return Some(pt[pt_idx]);
             }

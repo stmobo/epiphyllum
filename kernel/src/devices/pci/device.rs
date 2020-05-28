3ed82de6f835c60689131ec7bb948c29a8514a29
Sebastian <stmobo@gmail.com>
2020-05-27 21:06:32 -0500

    pci: add code for reading BARs

--- kernel/src/devices/pci/device.rs
+++ kernel/src/devices/pci/device.rs
@@ -1,10 +1,13 @@
+use alloc_crate::vec::Vec;
+use core::fmt;
+use core::fmt::Display;
+
 use super::config_space;
 use super::enhanced_cam;
 use super::PCIAddress;
+use crate::malloc::physical_mem;
 
-use alloc_crate::vec::Vec;
-
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct PCIDevice {
     address: PCIAddress,
     device_id: u16,
@@ -12,9 +15,86 @@ pub struct PCIDevice {
     major_class: u8,
     minor_class: u8,
     prog_if: u8,
+    bars: Vec<BAR>,
 }
 
 impl PCIDevice {
+    pub fn new(address: PCIAddress) -> PCIDevice {
+        let segment = address.segment();
+        let bus = address.bus();
+        let device = address.device();
+        let function = address.function();
+
+        let (major_class, minor_class, prog_if) = read_class(segment, bus, device, function);
+        let (vendor_id, device_id) = read_dev_vendor_id(segment, bus, device, function);
+
+        let data = unsafe { config_space::read_config(address, 12) };
+        let header_type = (((data >> 16) & 0xFF) as u8) & 0x7F;
+        let mut bars = Vec::new();
+
+        unsafe {
+            if header_type == 0x00 {
+                // Enumerate BARs:
+                let mut offset: u16 = 0x10;
+                while offset <= 0x24 {
+                    if let Some(bar) = BAR::new(address, offset) {
+                        if bar.large {
+                            offset += 8;
+                        } else {
+                            offset += 4;
+                        }
+
+                        bars.push(bar);
+                    } else {
+                        offset += 4;
+                    }
+                }
+            } else if header_type == 0x01 {
+                // PCI-to-PCI bridges have up to two BARs:
+
+                let test_bar1;
+                if let Some(bar) = BAR::new(address, 0x10) {
+                    test_bar1 = !bar.large;
+                    bars.push(bar);
+                } else {
+                    test_bar1 = true;
+                }
+
+                if test_bar1 {
+                    if let Some(bar) = BAR::new(address, 0x14) {
+                        bars.push(bar);
+                    }
+                }
+            }
+        }
+
+        println!(
+            "pci: enumerated device {}\npci:    Vendor: {:04x}\npci:    Device: {:04x}\npci:    Class: {:02x}.{:02x}.{:02x}",
+            address, vendor_id, device_id, major_class, minor_class, prog_if
+        );
+
+        if bars.len() > 0 {
+            println!("pci:    Device BARs:");
+            for bar in bars.iter() {
+                println!("pci:        {}", bar);
+
+                // if !bar.io {
+                //     physical_mem::reserve_page_range(bar.address, bar.size >> 12);
+                // }
+            }
+        }
+
+        PCIDevice {
+            address,
+            major_class,
+            minor_class,
+            prog_if,
+            vendor_id,
+            device_id,
+            bars,
+        }
+    }
+
     pub fn address(&self) -> PCIAddress {
         self.address
     }
@@ -45,6 +125,97 @@ impl PCIDevice {
 
     pub fn write_config(&mut self, offset: u16, value: u32) {
         unsafe { config_space::write_config(self.address, offset, value) }
+    }
+}
+
+#[derive(Debug)]
+pub struct BAR {
+    address: usize,
+    size: usize,
+    io: bool,
+    prefetch: bool,
+    large: bool, // if true, this is a 64-bit BAR
+    offset: u16,
+}
+
+impl BAR {
+    unsafe fn new(address: PCIAddress, offset: u16) -> Option<BAR> {
+        let d1 = config_space::read_config(address, offset);
+        let io = (d1 & 1) == 1;
+        let large = (d1 & 0b110) == 0b100;
+        let prefetch = (d1 & 0b1000) != 0;
+
+        let bar_addr: usize;
+        let size: usize;
+        if large {
+            let d2 = config_space::read_config(address, offset + 4);
+            bar_addr = ((d2 as usize) << 32) | ((d1 & 0xFFFF_FFF0) as usize);
+
+            config_space::write_config(address, offset, 0xFFFF_FFFF);
+            config_space::write_config(address, offset + 4, 0xFFFF_FFFF);
+
+            let s1 = config_space::read_config(address, offset);
+            let s2 = config_space::read_config(address, offset + 4);
+
+            let t = ((s2 as usize) << 32) | ((s1 & 0xFFFF_FFF0) as usize);
+            if t == 0 {
+                return None;
+            }
+
+            size = (!t) + 1;
+            config_space::write_config(address, offset, d1);
+            config_space::write_config(address, offset + 4, d2);
+        } else {
+            bar_addr = (d1 & 0xFFFF_FFF0) as usize;
+            config_space::write_config(address, offset, 0xFFFF_FFFF);
+
+            let s1 = config_space::read_config(address, offset);
+            let t = s1 & 0xFFFF_FFF0;
+            if t == 0 {
+                return None;
+            }
+
+            size = ((!t) + 1) as usize;
+            config_space::write_config(address, offset, d1);
+        }
+
+        Some(BAR {
+            address: bar_addr,
+            size,
+            io,
+            large,
+            prefetch,
+            offset,
+        })
+    }
+}
+
+impl Display for BAR {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "BAR #{}: ", (self.offset - 0x10) / 4)?;
+
+        if !self.io {
+            let pf = if self.prefetch {
+                "prefetchable"
+            } else {
+                "un-prefetchable"
+            };
+
+            if self.large {
+                write!(f, "64-bit {} memory at {:#018x} ", pf, self.address)?;
+            } else {
+                write!(f, "32-bit {} memory at {:#010x} ", pf, self.address)?;
+            }
+        } else {
+            write!(f, "I/O ports at {:#06x} ", self.address)?;
+        }
+
+        match self.size {
+            0..=0x400 => write!(f, "({}B)", self.size),
+            0x401..=0x100000 => write!(f, "({}K)", self.size >> 10),
+            0x100001..=0x40000000 => write!(f, "({}M)", self.size >> 20),
+            _ => write!(f, "({}G)", self.size >> 30),
+        }
     }
 }
 
@@ -83,32 +254,18 @@ fn enumerate_function(
     function: u8,
     out_list: &mut Vec<PCIDevice>,
 ) {
-    let (major_class, minor_class, prog_if) = read_class(segment, bus, device, function);
+    let address = PCIAddress::new(segment, bus, device, function);
+    let dev = PCIDevice::new(address);
+    let is_bridge = (dev.major_class == 0x06) && (dev.minor_class == 0x04);
 
-    if (major_class == 0x06) && (minor_class == 0x04) {
-        // PCI to PCI bridge
+    out_list.push(dev);
+
+    if is_bridge {
+        // enumerate devices behind bridge
         let secondary_bus =
             unsafe { config_space::read_config_split(segment, bus, device, function, 0x18) };
         let secondary_bus = ((secondary_bus >> 8) & 0xFF) as u8;
         enumerate_bus(segment, secondary_bus, out_list);
-    } else {
-        let address = PCIAddress::new(segment, bus, device, function);
-        let (vendor_id, device_id) = read_dev_vendor_id(segment, bus, device, function);
-        let device = PCIDevice {
-            address,
-            major_class,
-            minor_class,
-            prog_if,
-            vendor_id,
-            device_id,
-        };
-
-        println!(
-            "pci: enumerated device {}\npci:    Vendor: {:04x}\npci:    Device: {:04x}\npci:    Class: {:02x}.{:02x}.{:02x}",
-            address, device.vendor_id, device.device_id, device.major_class, device.minor_class, device.prog_if
-        );
-
-        out_list.push(device);
     }
 }
 
